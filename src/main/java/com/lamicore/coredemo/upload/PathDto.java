558ff99684daa868a9489d401e57fa59addd1bf3
Graceman Sam <adetoyesamuel63@gmail.com>
2024-04-01 23:24:14 +0100

    path

---
+++ src/main/java/com/lamicore/coredemo/upload/PathDto.java
@@ -0,0 +1,12 @@
+package com.lamicore.coredemo.upload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PathDto {
+    private String pathToJar;
+}
